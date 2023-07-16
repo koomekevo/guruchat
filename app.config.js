12ced9fd209328e130ba54aeff1bb21fc8700d11
koomekevo <koome.kevo@gurucrafts.net>
2023-07-16 18:04:43 +0300

    Install Firebase

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import "dotenv/config";
+
+export default {
   "expo": {
     "name": "guruchat",
     "slug": "guruchat",
@@ -25,6 +27,14 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGE_SENDER_ID,
+      appId: process.env.APP_ID
     }
   }
 }
