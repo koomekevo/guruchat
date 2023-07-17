4a1ae1ec7cbfe284805e809f1df7147e83f61d24
koomekevo <koome.kevo@gurucrafts.net>
2023-07-17 21:22:12 +0300

    Add LoginScreen.js and RegisterScreen.js

--- api/models/user.js
+++ api/models/user.js
@@ -37,7 +37,6 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model("User",userSchema);
-
-module.exports = User
+module.exports = User;
